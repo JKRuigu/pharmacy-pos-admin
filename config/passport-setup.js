fa88e7a92711e5e79ed3b69f91c6d9cb05487b67
JKRuigu <ruigukjohn.com>
2018-03-09 10:23:29 +0300

    correcting duplication of data in the db and updating if statements

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -25,48 +25,49 @@ passport.deserializeUser((id, done) => {
     	    clientSecret: keys.google.clientSecret,
     	    callbackURL: '/auth/google/redirect',
     	    passReqToCallback: true
-    	  },
-    	  function(req, accessToken, refreshToken, profile, done) {
-    	    	process.nextTick(function(){
+        	  },
+        	  function(req, accessToken, refreshToken, profile, done) {
+        	    	process.nextTick(function(){
 
-    	    		if(!req.user){
-    	    			User.findOne({'google.id': profile.id}, function(err, user){
-    		    			if(err)
-    		    				return done(err);
-    		    			if(user)
-    		    				return done(null, user);
-    		    			else {
-    		    				var newUser = new User();
-    		    				newUser.google.googleId = profile.id;
-    		    				newUser.google.token = accessToken;
-    		    				newUser.google.username = profile.displayName;
-    		    				newUser.google.email = profile.emails[0].value;
+        	    		if(!req.user){
+        	    			User.findOne({'google.id': profile.id}, function(err, user){
+        		    			if(err)
+        		    				return done(err);
+        		    			if(user)
+        		    				return done(null, user);
+        		    			else {
+        		    				var newUser = new User();
+        		    				newUser.google.id = profile.id;
+        		    				newUser.google.token = accessToken;
+        		    				newUser.google.name = profile.displayName;
+        		    				newUser.google.email = profile.emails[0].value;
 
-    		    				newUser.save(function(err){
-    		    					if(err)
-    		    						throw err;
-    		    					return done(null, newUser);
-    		    				})
-    		    			}
-    		    		});
-    	    		} else {
-    	    			var user = req.user;
-    	    			user.google.googleId = profile.id;
-      					user.google.token = accessToken;
-      					user.google.username = profile.displayName;
-      					user.google.email = profile.emails[0].value;
+        		    				newUser.save(function(err){
+        		    					if(err)
+        		    						throw err;
+        		    					return done(null, newUser);
+        		    				})
+        		    			}
+        		    		});
+        	    		} else {
+        	    			var user = req.user;
+        	    			user.google.id = profile.id;
+        					user.google.token = accessToken;
+        					user.google.name = profile.displayName;
+        					user.google.email = profile.emails[0].value;
 
-    					user.save(function(err){
-    						if(err)
-    							throw err;
-    						return done(null, user);
-    					});
-    	    		}
+        					user.save(function(err){
+        						if(err)
+        							throw err;
+        						return done(null, user);
+        					});
+        	    		}
 
-    	    	});
-    	    }
+        	    	});
+        	    }
 
-    	));
+        	));
+
 
 //FacebookStrategy
         passport.use(new FacebookStrategy({
