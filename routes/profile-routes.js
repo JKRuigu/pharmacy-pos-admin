1c836c9d6babc831ed3a8f617a94e3699081fdc4
JOE NYUGOH NGIGI <nyugoh@gmail.com>
2018-04-17 17:06:06 +0300

    Added code to check status change and activate the subscription.

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -4,14 +4,57 @@ const url = require('../config/keys').mongodb.dbURI;
 const Message = require('../models/Messages');
 const Update = require('../models/Updates');
 const Subscription = require('../models/Subscriptions');
+const moment = require('moment');
+const ObjectId = require('mongodb').ObjectId;
 
-//authenticate function
 const authCheck = (req,res,next)=>{
   if (!req.isAuthenticated()) {
     res.redirect('/users/login');
   }else if (req.user.isAdmin ===true) {
     res.redirect('/admin/admin');
   }else {
+    next();
+  }
+};
+
+const addSubscription = (req, res, next) =>{
+  if(req.body.status === 'COMPLETED') {
+    Subscription.findOne({_id: req.body.id}).then(transaction =>{
+      var now = moment();
+      var expiryDate = now;
+      var selected = transaction.item;
+      var qty = transaction.quantity;
+      if (selected === 'monthly'){
+        expiryDate = now.add(parseInt(qty), 'months');
+      } else if (selected === 'yearly'){
+        expiryDate = now.add(parseInt(qty), 'years');
+      } else if (selected === 'lifetime'){
+        expiryDate = now.add(10, 'years');
+      }
+      let data = {
+        expiryDate: expiryDate._d,
+        activationDate: moment()._d,
+        status: 1,
+        pesapalId: transaction.pesapalId,
+        subscriptionId: transaction._id
+      };
+      MongoClient.connect(url).then(client =>{
+        let db = client.db('pharmacy-pos');
+        db.collection('users').update(
+          {_id:ObjectId(transaction.customerId)},
+          {$push:{subscriptions: data}}).then( (user)=>{
+          next();
+        }).catch(error => {
+          res.status(404).json({message:error.message});
+        });
+        client.close();
+      }).catch( error => {
+        res.status(404).json({message:error.message});
+      });
+    }).catch(error =>{
+      console.log(error);
+    });
+  } else {
     next();
   }
 };
@@ -107,7 +150,7 @@ router.get('/*',authCheck,(req,res)=>{
   res.render('users/profile',{user:req.user});
 });
 
-router.post('/cart/pesapalUpdate', (req, res) =>{
+router.post('/cart/pesapalUpdate', addSubscription, (req, res) =>{
   Subscription.findOne({_id: req.body.id}).then( transaction =>{
     transaction.pesapalId = req.body.pesapalId;
     if(req.body.status === 'COMPLETED')
